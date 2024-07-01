4a11b2eea0a159e6dce83051b1299e1d096d3a5b
Robert Vitoriano <robertvitorianodev@gmail.com>
2024-07-01 17:27:34 +0900

    use generated nesting courses by category

--- graph/schema.resolvers.go
+++ graph/schema.resolvers.go
@@ -9,6 +9,25 @@ import (
 	"graphql-go-learning/graph/model"
 )
 
+// Courses is the resolver for the courses field.
+func (r *categoryResolver) Courses(ctx context.Context, obj *model.Category) ([]*model.Course, error) {
+	courses, coursesErr := r.CourseDB.FindByCategoryId(obj.ID)
+	if coursesErr != nil {
+		return nil, coursesErr
+	}
+	var coursesModel []*model.Course
+
+	for _, course := range courses {
+		coursesModel = append(coursesModel, &model.Course{
+			ID:          course.ID,
+			Name:        course.Name,
+			Description: &course.Description,
+		})
+	}
+
+	return coursesModel, nil
+}
+
 // CreateCategory is the resolver for the createCategory field.
 func (r *mutationResolver) CreateCategory(ctx context.Context, input model.NewCategory) (*model.Category, error) {
 	category, err := r.CategoryDB.Create(input.Name, *input.Description)
@@ -58,30 +77,11 @@ func (r *queryResolver) Categories(ctx context.Context) ([]*model.Category, erro
 	var categoriesModel []*model.Category
 
 	for _, category := range categories {
-		courses, coursesErr := r.CourseDB.FindByCategoryId(category.ID)
-		if coursesErr != nil {
-			return nil, coursesErr
-		}
-		var coursesModel []*model.Course
-
-		for _, course := range courses {
-			coursesModel = append(coursesModel, &model.Course{
-				ID:          course.ID,
-				Name:        course.Name,
-				Description: &course.Description,
-				Category: &model.Category{
-					ID:          category.ID,
-					Name:        category.Name,
-					Description: &category.Description,
-				},
-			})
-		}
 
 		categoriesModel = append(categoriesModel, &model.Category{
 			ID:          category.ID,
 			Name:        category.Name,
 			Description: &category.Description,
-			Courses:     coursesModel,
 		})
 	}
 
@@ -117,11 +117,15 @@ func (r *queryResolver) Courses(ctx context.Context) ([]*model.Course, error) {
 	return coursesModel, nil
 }
 
+// Category returns CategoryResolver implementation.
+func (r *Resolver) Category() CategoryResolver { return &categoryResolver{r} }
+
 // Mutation returns MutationResolver implementation.
 func (r *Resolver) Mutation() MutationResolver { return &mutationResolver{r} }
 
 // Query returns QueryResolver implementation.
 func (r *Resolver) Query() QueryResolver { return &queryResolver{r} }
 
+type categoryResolver struct{ *Resolver }
 type mutationResolver struct{ *Resolver }
 type queryResolver struct{ *Resolver }
